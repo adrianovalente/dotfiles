25bbbf1eb79bd59db543f497800b71d9b320d71a
Adriano Valente <dri9595@gmail.com>
2017-02-17 10:57:23 -0200

    scripts to add spotify to PS1

---
+++ spotify.sh
@@ -0,0 +1,9 @@
+function currentPlaying () {
+  if [[ $(osascript -e 'tell application "Spotify" to player state as string' 2> /dev/null) == *"playing"* ]]; then
+    artist=`osascript -e 'tell application "Spotify" to artist of current track as string' 2> /dev/null`;
+    track=`osascript -e 'tell application "Spotify" to name of current track as string' 2> /dev/null`;
+    echo " "$track" ("$artist")"
+  fi
+}
+
+ PS1="\n\[\033[01;30m\]\h\[\033[38;5;85m\]\$(currentPlaying)\[\033[01;39m\] \W \[\033[31m\]\$(parse_git_branch) \[\033[01;31m\]$\[\033[00m\] "
