a8c9fce9cd84bc7803f5b9a5349ffb42fef96464
drico <dri9595@gmail.com>
2017-03-07 16:29:57 -0300

    added bash_custom

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -5,4 +5,5 @@ git pull origin master > /dev/null
 echo 'Rewriting system dotfiles...'
 ls | grep symlink | sed -e 's/.symlink//g' | xargs -I {} sh -c 'cp {}.symlink ~/.{}'
 
+source ~/.bash_profile
 echo 'Done'
