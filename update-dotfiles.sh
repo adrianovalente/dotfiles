7e7585190c93efb52b4bb82315334422acca61b6
Adriano Valente <dri9595@gmail.com>
2017-03-07 11:15:42 -0300

    added update script

---
+++ update-dotfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo 'Pulling .dotfiles from master...'
+git pull origin master > /dev/null
+
+echo 'Rewriting system dotfiles...'
+ls | grep symlink | sed -e 's/.symlink//g' | xargs -I {} sh -c 'cp {}.symlink ~/.{}'
+
+echo 'Done'
