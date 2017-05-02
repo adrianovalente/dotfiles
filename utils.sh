b1e5214f3b4c56110b5ff52723390da7db73d4cd
drico <dri9595@gmail.com>
2017-05-02 16:51:47 -0300

    fixed pp function

--- utils.sh
+++ utils.sh
@@ -8,10 +8,11 @@ alias sp='sudo pkill'
 alias spa='spn; spm; spr'
 alias s='gst'
 
+alias v='vim'
 alias r='git fetch origin; git reset --hard origin/dev'
 alias desktop='cd ~/Desktop'
 alias downloads='cd ~/Downloads'
 alias pp='g push origin $(current_branch)'
-alias pp='pp -f'
+alias PP='pp -f'
 alias gh="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 alias cl='clear'
