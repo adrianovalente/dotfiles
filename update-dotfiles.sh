9d01a46e755a4d1818789206e8ae6af7b59b67a6
drico <dri9595@gmail.com>
2019-04-02 17:22:58 -0300

    added custom oh-my-zsh config

--- update-dotfiles.sh
+++ update-dotfiles.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
 echo 'Pulling .dotfiles from master...'
 git pull origin master > /dev/null
 
 echo 'Rewriting system dotfiles...'
 ls | grep symlink | sed -e 's/.symlink//g' | xargs -I {} sh -c 'cp {}.symlink ~/.{}'
+
+if [ -d $HOME/.oh-my-zsh ]; then
+  echo 'Updating oh-my-zsh custom configs...'
+  cp -R ~/dotfiles/.oh-my-zsh/custom ~/.oh-my-zsh/custom
+fi
 
 exec $SHELL
