db55f38dc63236a5e49426960ab9bbca87e86c74
drico <dri9595@gmail.com>
2019-04-02 17:18:45 -0300

    removed old code

--- utils.sh
+++ utils.sh
@@ -1,8 +1,3 @@
 alias s='gst'
 alias v='vim'
-alias r='git fetch origin; git reset --hard origin/dev'
-alias desktop='cd ~/Desktop'
-alias downloads='cd ~/Downloads'
-alias pp='g push origin $(current_branch)'
-alias PP='pp -f'
 alias gh="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
