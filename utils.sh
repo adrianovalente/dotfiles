4dc2fddf9d7e046355fc943eee93440703a47175
drico <dri9595@gmail.com>
2019-04-02 17:17:05 -0300

    good bye revmob 😥

--- utils.sh
+++ utils.sh
@@ -1,12 +1,4 @@
-alias ]='auto_mfa; nodemon -e '"'"'.js|.ejs'"'"' app.js' 
-alias gap='git add -p'
-
-alias spn='sudo pkill node'
-alias spm='sudo pkill mongo'
-alias spr='sudo pkill redis'
-alias spa='spn; spm; spr'
 alias s='gst'
-
 alias v='vim'
 alias r='git fetch origin; git reset --hard origin/dev'
 alias desktop='cd ~/Desktop'
@@ -14,4 +6,3 @@ alias downloads='cd ~/Downloads'
 alias pp='g push origin $(current_branch)'
 alias PP='pp -f'
 alias gh="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
-alias cl='clear'
