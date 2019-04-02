977e63ac3726a1f8613072e996865f8d0ec89aec
drico <dri9595@gmail.com>
2019-04-02 17:30:55 -0300

    added command to automatically update dotfiles

--- utils.sh
+++ utils.sh
@@ -1,3 +1,4 @@
 alias s='gst'
 alias v='vim'
 alias gh="git log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+alias update-dotfiles="~/dotfiles/update-dotfiles.sh"
