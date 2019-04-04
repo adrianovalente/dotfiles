05c6fd0e1e06759e5f2413c8ffcd35ea9923365b
drico <dri9595@gmail.com>
2019-04-04 14:00:22 -0300

    fixed recursive search

--- oh-my-zsh/custom/config.zsh
+++ oh-my-zsh/custom/config.zsh
@@ -4,3 +4,6 @@ source $HOME/dotfiles/bash/utils
 source $HOME/dotfiles/bash/spotify
 
 bindkey -v
+
+# makes ^R work as recursive search (instead of redo)
+bindkey "^R" history-incremental-search-backward
